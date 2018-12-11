18f12331e8714e0b2b40c9af8c7b6a1f73322db7
Fu Wei <softbaddog@gmail.com>
2018-12-11 20:35:15 +0800

    增删改查完成

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const indexRouter = require('./routes/index');
 const devicesRouter = require('./routes/devices');
 const adminRouter = require('./routes/admin');
 
+const auth = require('./iotplatform/auth');
+const sub = require('./iotplatform/sub');
+
 const app = express();
 
 mongoose.connect('mongodb://localhost/AAA', {
@@ -26,13 +29,23 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/devices', devicesRouter);
 app.use('/admin', adminRouter);
+
+// After fetchAccessToken, subscribe notifyType
+auth.fetchAccessToken().then(() => {
+  console.log("subscribe is coming...");
+  for (const item of sub.notifyTypeList) {
+    if (item.enabled) {
+      sub.subscribe(auth.loginInfo, item.notifyType);
+    }
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,129 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const _ = require('underscore');
+const Device = require('../models/device');
 
 /* GET users listing. */
 router.get('/list', function (req, res, next) {
-  res.render('list.pug', {
-    title: "Admin List"
+  Device.fetch(function (err, devices) {
+    if (err) console.log(err);
+    res.format({
+      html: () => {
+        res.render('list', {
+          title: 'NOS Cafe',
+          desc: 'Coffee Machines Administrator',
+          devices: devices
+        });
+      },
+      json: () => {
+        res.json({
+          status: "0",
+          msg: "",
+          result: {
+            count: devices.count,
+            data: devices
+          }
+        });
+      }
+    });
   });
 });
 
 router.get('/device', function (req, res, next) {
-  res.render('admin.pug', {
-    title: "Admin device"
+  res.render('admin', {
+    title: 'Device Management Admin',
+    device: [{
+      nodeId: '',
+      nodeName: ''
+    }]
+  });
+});
+
+router.get('/update/:id', function (req, res) {
+  var id = req.params.id;
+
+  if (id) {
+    Device.findByIdAndDelete(id, function (err, device) {
+      res.render('admin', {
+        title: 'Admin Update',
+        device: device
+      });
+    });
+  }
+});
+
+/* Create a device */
+router.post('/device/new', function (req, res) {
+  var id = req.body.device._id;
+  var deviceObj = req.body.device;
+  var _device;
+  if (id !== '') {
+    Device.findById(id, function (err, device) {
+      if (err) {
+        console.log(err);
+      }
+
+      _device = _.extend(device, deviceObj);
+      _device.save(function (err, device) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.format({
+            html: () => {
+              res.redirect('/devices/' + device._id);
+            },
+            json: () => {
+              res.json({
+                status: "0",
+                msg: "",
+
+                result: device
+              });
+            }
+          });
+        }
+      });
+    });
+  } else {
+    _device = new Device({
+      nodeId: deviceObj.nodeId,
+      nodeName: deviceObj.nodeName
+    });
+    _device.save(function (err, device) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.format({
+          html: () => {
+            res.redirect('/devices/' + device._id);
+          },
+          json: () => {
+            res.json({
+              status: "0",
+              msg: "",
+              result: device
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+/* Delete a device */
+router.delete('/list', function (req, res, next) {
+  var id = req.query.id;
+  Device.findByIdAndRemove(id, function (err, device) {
+    if (err) {
+      console.log("xxxxxx");
+      console.log(err);
+    } else {
+      console.log("oooooo");
+      console.log(device);
+      res.json({
+        success: 1
+      });
+    }
   });
 });
 
